9f50b916309068b606e7598e7c4f6fe2f5bd8394
michischirmer <64130395+michischirmer@users.noreply.github.com>
2020-09-24 10:27:07 +0200

    added the helper file from CS50

--- application.py
+++ application.py
@@ -3,9 +3,16 @@
 
 from flask import Flask, render_template, jsonify
 from flask_socketio import SocketIO, emit
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+from helpers import apology, login_required
 
 app = Flask(__name__, template_folder='templates', static_url_path='/static/', static_folder='static')
 app.config['SECRET_KEY'] = 'ines'
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 socketio = SocketIO(app)
 
 @app.route('/')
@@ -22,3 +29,16 @@ def receive_message(data):
 
 if __name__ == '__main__':
 	socketio.run(app, debug=True)
+
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
