cee4cda6eb0a2f8fdc5afdf60f3f48abe3876d90
michischirmer <64130395+michischirmer@users.noreply.github.com>
2020-09-24 15:55:46 +0200

    created basic infrastructure for page

---
+++ app.py
@@ -0,0 +1,148 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup, usd
+
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+
+@login_required
+@app.route("/")
+def index():
+    user_id = session['user_id']
+    return render_template("index.html")
+
+
+@login_required
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+
+@login_required
+@app.route("/inbox")
+def inbox():
+    return render_template("inbox.html")
+
+
+@login_required
+@app.route("/send")
+def send():
+    return render_template("send.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            # return apology("invalid username and/or password", 403)
+            return render_template("login.html", error=True)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "GET":
+        return render_template("register.html")
+    elif request.method == "POST":
+        name = request.form.get("username")
+        password = request.form.get("password")
+
+        # user did not provide a name, a password or a confirmation password
+        if not name or not password or not request.form.get("confirmation"):
+            return apology("You must provide an input!")
+
+        # user did not provide to identical passwords
+        if password != request.form.get("confirmation"):
+            return apology("Passwords must be the same.")
+
+        if db.execute("SELECT username FROM users WHERE username=:username;", username=name):
+            return apology("Username is already taken. Please choose another.")
+
+        db.execute("INSERT INTO users (username, hash) VALUES (:username, :password);",
+                   username=name, password=generate_password_hash(password))
+        return redirect("/login")
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
